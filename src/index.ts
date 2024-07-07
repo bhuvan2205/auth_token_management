d0553727b4e2228db24e58d6f2d0176ed002c39a
bhuvan2205 <bhuvancs2205@gmail.com>
2024-07-07 11:24:06 +0530

    Feat: Add routes in Home

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ app.get("/", (c) => {
         <li style="margin: 10px;">Get User Session <a href="/api/users/session">GET</a></li>
         <li style="margin: 10px;">Create User Session <a href="/api/users/session">POST</a></li>
         <li style="margin: 10px;">Delete User Session <a href="/api/users/session">DELETE</a></li>
+        <li style="margin: 10px;">Get User Posts <a href="/api/posts">GET</a></li>
+        <li style="margin: 10px;">Create New Post <a href="/api/posts">POST</a></li>
+        <li style="margin: 10px;">Get Single Post <a href="/api/posts/:id">GET</a></li>  
+        <li style="margin: 10px;">Delete Post <a href="/api/posts/:id">DELETE</a></li>  
     </ul>
   `;
   return c.html(routes);
